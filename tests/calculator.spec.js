19bafc6a69aafeac1f55098cac1a9dd226d037c9
JankaPenzes <penzesjanka@gmail.com>
2024-03-11 20:11:15 +0200

    - parking lot test modification

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -12,6 +12,6 @@ const test = base.test.extend({
     test("@my-test-01-Filling in all the fields correctly", async ({ page, mainPage }) => {
       await mainPage.infoEntry("2", "March","2025","12","00");
       await mainPage.infoExit("24", "March","2026","15","00");
-      await mainPage.costCalculator();
+      await mainPage.costCalculator("Short-Term Parking");
     });
 });
