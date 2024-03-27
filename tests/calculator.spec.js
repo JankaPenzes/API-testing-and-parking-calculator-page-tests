6a404f094f83298ebbd045570cea4c41aa35d177
JankaPenzes <penzesjanka@gmail.com>
2024-03-27 10:29:49 +0200

    -locators changed, instead of while loop and clicking i used fill -first test running

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -10,8 +10,8 @@ const test = base.test.extend({
   });
   test.describe("Testing cost calculator", () => {
     test("@my-test-01-Filling in all the fields correctly", async ({ page, mainPage }) => {
-      await mainPage.infoEntry("2", "March","2025","12","00");
-      await mainPage.infoExit("24", "March","2026","15","00");
-      await mainPage.costCalculator("Short-Term Parking");
+      await mainPage.infoEntry("2025-03-20","12","00");
+      await mainPage.infoExit("2025-03-21","15","00");
+      await mainPage.costCalculator("Economy Parking");
     });
 });
